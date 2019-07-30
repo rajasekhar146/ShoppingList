8c7f83674407181deda6faee22b6ce8611e3ef72
rajasekhar <rajasekharmtiet@gmail.com>
2019-07-30 10:43:31 +0530

    second commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import AppNavbar from './Components/AppNavbar'
+
+function App() {
+  return (
+    <div className="App">
+        <AppNavbar />
+    </div>
+  );
+}
+
+export default App;
